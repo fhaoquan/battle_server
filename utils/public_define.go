07fabcf4a1d0e490ca62e18bfc01ac408ffb468e
123youyouer <179261275@qq.com>
2017-09-15 15:10:54 +0800

    1

--- utils/public_define.go
+++ utils/public_define.go
@@ -3,3 +3,12 @@ package utils
 const MaxPktSize  				=1024;
 const MaxRoomSize 				=10;
 const UdpListenStart			=10000;
+
+const CMD_pingpong =uint8(0);
+const CMD_battle_wating_start =uint8(1);
+const CMD_battle_all =uint8(2);
+const CMD_unit_movment =uint8(3);
+const CMD_create_unit =uint8(4);
+const CMD_attack_done =uint8(5);
+const CMD_attack_start =uint8(6);
+const CMD_battle_start =uint8(7);
--- utils/response.go
+++ utils/response.go
@@ -3,14 +3,6 @@ package utils
 import (
 	"encoding/binary"
 )
-const CMD_pingpong =uint8(0);
-const CMD_battle_wating_start =uint8(1);
-const CMD_battle_all =uint8(2);
-const CMD_unit_movment =uint8(3);
-const CMD_create_unit =uint8(4);
-const CMD_attack_done =uint8(5);
-const CMD_attack_start =uint8(6);
-const CMD_battle_start =uint8(7);
 
 type KcpRes struct{
 	ICachedData;
